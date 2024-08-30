044f9822b57262151b71362ad1bca025904464da
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-08-30 16:42:06 +0200

    make sure one of the child extensions is installed

---
+++ ext_tables.php
@@ -0,0 +1,7 @@
+<?php
+defined('TYPO3') || die('Access denied.');
+
+if (!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('powermail_jira_issues')
+    && !\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('powermail_jiraonpremise_issues')) {
+    throw new \Exception('EXT:powermail_jira_issues or EXT:powermail_jiraonpremise_issues must be installed!');
+}

bdb372412bd4436370d957a81c84d73ae14ef4ee
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-09-30 16:58:28 +0200

    add description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,17 +2,18 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Powermail JIRA Base',
-    'description' => 'Post powermail form submissions as jira issues',
+    'description' => 'Base extension - Post powermail form submissions as jira issues',
     'category' => 'misc',
     'author' => 'Thomas Rawiel',
     'author_email' => 'thomas.rawiel@gmail.com',
     'state' => 'beta',
     'clearCacheOnLoad' => 0,
-    'version' => '0.1.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.9.99',
+            'typo3' => '12.0.0-12.99.99',
             'powermail' => '10.0.0-12.99.99',
+            'extender' => '10.0.0-10.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],

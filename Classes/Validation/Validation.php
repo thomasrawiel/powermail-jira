3d3552aea12e436e534f6e102e504175d531f2e3
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-10-09 10:19:29 +0200

    [bugfix] adapt issueconfig validation to latest config structure

--- Classes/Validation/Validation.php
+++ Classes/Validation/Validation.php
@@ -21,11 +21,23 @@ class Validation
         if (empty($tca['label'])) {
             throw new \Exception('Label is required');
         }
-        if (empty($project_key)) {
-            throw new \Exception('Project key is required');
-        }
+
         if (strlen($tca['value']) > 20) {
             throw new \Exception('Configuration key is too long (max 20 chars)');
+        }
+
+        return self::validateIssueConfiguration($issueConfiguration);
+    }
+
+    protected static function validateIssueConfiguration(?array $issueConfiguration): bool
+    {
+        if (empty($issueConfiguration)) {
+            return false;
+        }
+        foreach ($issueConfiguration as $key => $issueConfig) {
+            if (empty($issueConfig['project_key'])) {
+                throw new \Exception('Project key is required in issue configuration #' . $key);
+            }
         }
 
         return true;

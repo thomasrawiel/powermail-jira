abf207e5d63fca7fe238a942daa858490fc4e01a
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2026-05-04 10:14:30 +0200

    [chore] update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,12 +8,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'thomas.rawiel@gmail.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.5.2',
+    'version' => '1.6.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.99.99',
-            'powermail' => '10.0.0-12.99.99',
-            'extender' => '10.0.0-10.99.99',
+            'typo3' => '13.0.0-13.99.99',
+            'powermail' => '13.0.0-13.99.99',
+            'extender' => '10.0.0-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
